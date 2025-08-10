8b14bc3d7c6e2edc59921261c2e299ad2669cd71
Josmar Martins <162395427+JOSMARMARTINS@users.noreply.github.com>
2025-08-10 00:44:08 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+
+#Criando variável que recebe e cria a nossa janela. Posso chamar essa variável de screen
+window = pygame.display.set_mode(size=(600, 480))
+print('Setup End')
+
+print('Loop Start')
+while True:  #Laço indinico para mante minha janela aberta sem fechar
+    # check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # close Window
+            quit()  # end pygame
