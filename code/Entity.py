33a9503b82ae2150c4ee39230eef61611eb956f5
Josmar Martins <162395427+JOSMARMARTINS@users.noreply.github.com>
2025-08-13 00:39:37 -0300

    Creating menu intarations Criating BC class Creating Factory Level 1 Background implamentation: parallax

--- code/Entity.py
+++ code/Entity.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 
-class Entity:
-    def __init__(self):
-        self.name = None
-        self.surf = None
-        self.rect = None
+import pygame.image
 
+
+class Entity(ABC):
+    def __init__(self, name: str, position: tuple):
+        self.name = name
+        self.surf = pygame.image.load('./assent/' + name + '.png') # Carreagar imagem
+        self.rect = self.surf.get_rect(left=position[0], top=position[1]) # Fazer o retangulo, posição livre
+        self.speed = 0 # velocidade
+
+    @abstractmethod
     def move(self, ):
         pass
+
--- code/Menu.py
+++ code/Menu.py
@@ -4,7 +4,7 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT, COLOR_ORANGE, COLOR_WHITE, MENU_OPTION
+from code.Const import WIN_WIDTH, WIN_HEIGHT, COLOR_ORANGE, COLOR_WHITE,COLOR_YELLOW, MENU_OPTION
 
 class Menu:
     def __init__(self, window):
@@ -13,39 +13,68 @@ class Menu:
         self.rect =  self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./assent/Menu.mp3') #  Carregando música
         pygame.mixer_music.play(-1) #  Para tocar a música. O -1 deixa ala repedindo infinitamente
         while True:
+            # DRAW IMAGES (desenhe as imagens)
             self.window.blit(source=self.surf, dest=self.rect)  # desenha a imagem no retângulo
             self.menu_text(
                 50,  # tamanho da fonte
                 "Mountain",  # texto
-                COLOR_WHITE,  # cor RGB
+                COLOR_ORANGE,  # cor RGB
                 (WIN_WIDTH / 2, 70)  # posição X, Y
             )
             self.menu_text(
                 50,  # tamanho da fonte
                 "Shooter",  # texto
-                COLOR_WHITE,  # cor RGB
+                COLOR_ORANGE,  # cor RGB
                 (WIN_WIDTH / 2, 120)  # posição X, Y
             )
 
             #  CRIA O MENU COM UM ESPARÇO DE 25 NO EIXO X
             for i in range(len(MENU_OPTION)):
-                self.menu_text(
-                    20,  # tamanho da fonte
-                    MENU_OPTION[i],  # texto
-                    COLOR_WHITE,  # cor RGB
-                    ((WIN_WIDTH / 2), 200 + 25 * i)  # posição X, Y
-                )
-
+                if i == menu_option:
+                    self.menu_text(
+                        20,  # tamanho da fonte
+                        MENU_OPTION[i],  # texto
+                        COLOR_YELLOW,  # cor RGB
+                        ((WIN_WIDTH / 2), 200 + 25 * i)  # posição X, Y
+                    )
+                else:
+                    self.menu_text(
+                        20,  # tamanho da fonte
+                        MENU_OPTION[i],  # texto
+                        COLOR_WHITE,  # cor RGB
+                        ((WIN_WIDTH / 2), 200 + 25 * i)  # posição X, Y
+                    )
             pygame.display.flip()  # para atualizar imagem na tela
+
 
             # check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # close Window
                     quit()  # end pygame
+                #   Seleção no menu para baixo
+                if event.type == pygame.KEYDOWN: # verifica se é tecla para baico DOWN KEY
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) -1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+
+                    #   seleção do menu para cima
+                    if event.key == pygame.K_UP: # Verfica se é telcla para cima UP KEY
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) -1
+
+                    if event.key == pygame.K_RETURN: # TECLA ENTER
+                        return MENU_OPTION[menu_option]
+
+
 
     #  Método com a as fontes
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
