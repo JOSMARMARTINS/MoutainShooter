25a907be74fdae54f6208c3152777dfdf758f10f
Josmar Martins <162395427+JOSMARMARTINS@users.noreply.github.com>
2025-08-13 23:36:00 -0300

    Fixing imports in Level.py file

--- code/Entity.py
+++ code/Entity.py
@@ -8,7 +8,7 @@ import pygame.image
 class Entity(ABC):
     def __init__(self, name: str, position: tuple):
         self.name = name
-        self.surf = pygame.image.load('./assent/' + name + '.png') # Carreagar imagem
+        self.surf = pygame.image.load('./assent/' + name + '.png').convert_alpha() # Carreagar imagem
         self.rect = self.surf.get_rect(left=position[0], top=position[1]) # Fazer o retangulo, posição livre
         self.speed = 0 # velocidade
 
--- code/Level.py
+++ code/Level.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
+import sys
 
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import COLOR_WHITE, WIN_HEIGHT
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -13,11 +18,32 @@ class Level:
         self.game_mode = game_mode #  modo de jogo
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.timeout = 20000 # 20 segundos
 
     def run(self):
+        pygame.mixer_music.load(f'./assent/{self.name}.mp3') #  Inserindo música no Level 1
+        pygame.mixer_music.play(-1) #  mandei a música rodar indefinidamente
+        clock = pygame.time.Clock() #  Vai garatir que vai rodar com 60 FPS em qualquer máquina
         while True:
+            clock.tick(60) # quantidade de FPS
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pygame.event.get(): #gerenciador de evento para fechar o Leve1
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # printed text
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', COLOR_WHITE, (10, 5)) # motrar o tempo de duração da fase
+            self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35)) # faz o print do clocl na tela. Impreme o FPS do game
+            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20)) # mostra quantas entidade criadas na tela
             pygame.display.flip()
+
         pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
