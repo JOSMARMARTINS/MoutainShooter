bb363c4bab0fffb00f2f5add09d1ccf1cd0280fd
Josmar Martins <162395427+JOSMARMARTINS@users.noreply.github.com>
2025-08-13 23:52:09 -0300

    Fixing imports in Level.py file

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -15,3 +15,6 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+
+            case 'Player1':
+                return Player('Player')
